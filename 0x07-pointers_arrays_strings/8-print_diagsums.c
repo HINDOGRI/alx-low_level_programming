5326ff9cb57aa13dd8abe7fcf35e194ff215d98c
HINDOGRI <hindogri723@gmail.com>
2023-09-25 14:35:57 -0700

    Task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a
- *              square matrix of integers
+ *		square matrix of integers
  * @a: pointer to the first element of the matrix
  * @size: size of the matrix
  */
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
-	for (i = 0; < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * (size + 1)];
 		sum2 += a[(i + 1) * (size - 1)];
